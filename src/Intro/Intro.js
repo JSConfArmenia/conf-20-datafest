951a6fe3826b308705599b3d425509b13d4cf20a
Vazgen Barfyan <vbarfyan@gmail.com>
2020-03-15 23:05:19 +0400

    Added postpone information.

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -6,8 +6,22 @@ const Intro = () => (
   <section id="intro" className="Intro">
     <div className="IntroContainer">
       <div className="Info">
-        <p>American University of Armenia</p>
-        <p>April 9-11, 2020</p>
+        <p>
+DataFest is postponed due to the situation with
+          <span style={{
+            paddingLeft: 3,
+            fontSize: 30,
+            color: 'rgb(217, 29, 189)',
+            whiteSpace: 'nowrap',
+          }}
+          >
+Corona virus
+          </span>
+.
+        </p>
+        <p> Exact dates will be posted later.</p>
+        {/* <p>American University of Armenia</p> */}
+        {/* <p>April 9-11, 2020</p> */}
       </div>
       <h1>DataFest Yerevan</h1>
     </div>
